0f8b97f04ed2d39c0311fd2c6eed09ceccdce9c9
deepakhawale3 <deepakhawale3@gmail.com>
2024-01-23 00:32:13 +0530

    Minor changes Done

--- app/Http/Controllers/Admin/AllbloggerController.php
+++ app/Http/Controllers/Admin/AllbloggerController.php
@@ -12,6 +12,10 @@ class AllbloggerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $bloggers = User::where('role', 'blogger')
--- app/Http/Controllers/Blogger/BlogController.php
+++ app/Http/Controllers/Blogger/BlogController.php
@@ -11,10 +11,10 @@ use App\Models\Comment;
 use Illuminate\Support\Facades\Auth;
 class BlogController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $bloggerId = Auth::id();
