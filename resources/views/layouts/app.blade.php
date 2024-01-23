bd0510743232f846e11f9a5a919698f3cdd2e59e
deepakhawale3 <deepakhawale3@gmail.com>
2024-01-23 08:44:32 +0530

    Timezone Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,9 @@
                 </div>
             </div>
             <div class="col-md-4 text-right d-none d-md-block">
-                Monday, January 01, 2045
+                {{ now()->setTimezone('Asia/Kolkata')->format('l, F d, Y H:i:s') }}
             </div>
+
             
         </div>
         <div class="row align-items-center py-2 px-lg-5">
